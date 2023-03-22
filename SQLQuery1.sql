ce4d034db8fcc0aabd51218c7b727625e85a2aa7
RosileneTi <rosa-clemente@hotmail.com>
2023-03-22 09:32:12 -0300

    Implementação
    Script banco de dados

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,7 +11,7 @@ INSERT INTO Permissao(Id, Descricao)VALUES(8,'Excluir grupo de usuário')
 INSERT INTO Permissao(Id, Descricao)VALUES(9,'Adicionar permissão a um grupo de usuário')
 INSERT INTO Permissao(Id, Descricao)VALUES(10,'Adicionar grupo de usuário a um usuário')
 
-SELECT * FROM Permissao;
+ SELECT * FROM GruppoUsuario;
  INSERT INTO GrupoUsuario(NomeGrupo) VALUES('Gerente')
  INSERT INTO GrupoUsuario(NomeGrupo) VALUES('Vendedo')
  INSERT INTO GrupoUsuario(NomeGrupo) VALUES('Estoquista')
@@ -19,22 +19,31 @@ SELECT * FROM Permissao;
  INSERT INTO GrupoUsuario(NomeGrupo) VALUES('Operador de caixa')
 
  SELECT * FROM GrupoUsuario
- SELECT * FROM |GrupoUsuario
- SELECT* FROM Usuario
- SELECT * FROM  PermissaoGrupoUsuario
- SELECT * from UsuarioGrupoUsuario
+ 
+ SELECT * FROM permissaoGrupoUsuario
+ SELECT * FROM UsuarioGrupoUsuario
+
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,1)
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,2)
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,3)
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,4)
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,5)
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,6)
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,7)
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,8)
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,9)
+INSERT INTO Permissao(IdGrupoUsuario , IdPermissao)VALUES(18,10)
+					  
+
+ INSERT INTO UsuarioGrupo(IdGrupoUsuario,Idusuario) VALUES(18,24)
  
  
- 
-SELECT * FROM GrupoUsuario
+SELECT * FROM Usuario
+SELECT * FROM UsuarioGrupoUsuario
 
+DECLARE @IdUsuario INT =14
+DECLARE @IdPermissao INT = 10
 
-
-
-
-
- DECLARE @IdUsuario INT = 14
- DECLARE @IdPermissao INT = 10
 
  SELECT 1 FROM PermissaoGrupoUsuario
  INNER JOIN UsuarioGrupoUsuario ON PermissaoGrupoUsuario.IdGrupoUsuario = UsuarioGrupoUsuario.IdGrupoUsuario
