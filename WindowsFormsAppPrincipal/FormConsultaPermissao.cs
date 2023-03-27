d2a9e06d14f4979984763392c85cbaae23c0243b
RosileneTi <rosa-clemente@hotmail.com>
2023-03-26 22:02:40 -0300

    correção formPermissao

---
+++ WindowsFormsAppPrincipal/FormConsultaPermissao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppPrincipal
+{
+    public partial class FormPermissao : Form
+    {
+        public FormPermissao()
+        {
+            InitializeComponent();
+        }
+
+        private void permissaoBindingNavigator_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
