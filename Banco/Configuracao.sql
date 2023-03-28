0a2e442797b3e4438f478e0515f786eb3c193987
Luiz005fil <rosa-clemente@hotmail.com>
2023-03-28 14:29:42 -0300

    alteração banco de dados

--- Banco/Configuracao.sql
+++ Banco/Configuracao.sql
@@ -109,16 +109,6 @@ INSERT INTO UsuarioGrupoUsuario(IdUsuario,IdGrupoUsuario) VALUES(11,6)
 INSERT INTO UsuarioGrupoUsuario(IdUsuario,IdGrupoUsuario) VALUES(12,4)
 INSERT INTO UsuarioGrupoUsuario(IdUsuario,IdGrupoUsuario) VALUES(11,2)
 
-INSERT INTO Permissao(Id, Descricao)VALUES(1,'Visualizar usuário')
-INSERT INTO Permissao(Id, Descricao)VALUES(2,'Usuário cadastrar')
-INSERT INTO Permissao(Id, Descricao)VALUES(3,'Alterar usuário')
-INSERT INTO Permissao(Id, Descricao)VALUES(4,'Excluir usuário')
-INSERT INTO Permissao(Id, Descricao)VALUES(5,'Visualizar grupo de usuário')
-INSERT INTO Permissao(Id, Descricao)VALUES(6,'Cadastrar grupo de usuário')
-INSERT INTO Permissao(Id, Descricao)VALUES(7,'Alterar grupo de usuário')
-INSERT INTO Permissao(Id, Descricao)VALUES(8,'Excluir grupo de usuário')
-INSERT INTO Permissao(Id, Descricao)VALUES(9,'Adicionar permissão a um grupo de usuário')
-INSERT INTO Permissao(Id, Descricao)VALUES(10,'Adicionar grupo de usuário a um usuário')
 
 
 
